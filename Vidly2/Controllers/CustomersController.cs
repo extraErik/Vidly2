d139a41f235f46f124142d7b4d62a4087c5da4e3
Erik Rygg <erygg01@live.com>
2020-11-10 12:26:07 -0600

    course section2 MVC fundamentals completed

---
+++ Vidly2/Controllers/CustomersController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly2.Models;
+
+namespace Vidly2.Controllers
+{
+    public class CustomersController : Controller
+    {
+        public ActionResult Index()
+        {
+
+            var customers = new List<Customer>
+            {
+                new Customer { Id = 1, Name = "Customer One" },
+                new Customer { Id = 2, Name = "Customer Two" }
+            };
+
+            return View(customers);
+        }
+
+        [Route("customers/details/{id}")]
+        public ActionResult Details(int id)
+        {
+            var customers = new List<Customer>
+            {
+                new Customer { Id = 1, Name = "Customer One" },
+                new Customer { Id = 2, Name = "Customer Two" }
+            };
+
+            var specifiedCustomer = customers.Find(item => item.Id == id);
+            if (specifiedCustomer != null)
+            {
+                return View(specifiedCustomer);
+            }
+            else
+            {
+                return HttpNotFound();
+            }
+
+        }
+    }
+}
---
+++ Vidly2/Controllers/MoviesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly2.Models;
+
+namespace Vidly2.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var movies = new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Despicable Me" },
+                new Movie { Id = 2, Name = "Ice Age" },
+                new Movie { Id = 3, Name = "Zootopia" }
+            };
+
+            return View(movies);
+        }
+    }
+}
